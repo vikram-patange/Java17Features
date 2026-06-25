1e6e8de7cc4d3cc15791b82d0f7daefc900fff14
vikram patange <vikrampatange11@gmail.com>
2026-06-25 09:58:12 +0530

    Initial commit

--- src/NonRepetedChar.java
+++ src/NonRepetedChar.java
@@ -1,10 +1,8 @@
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class NonRepetedChar {
     public static void main() {
@@ -35,39 +33,80 @@ public class NonRepetedChar {
                 .collect(Collectors.groupingBy(Function.identity(),
                         LinkedHashMap::new, Collectors.counting()));
 
-           StringBuffer s11 = new StringBuffer("Vikram");
-           String s12="Vikram";
-           String s13="Vikram";
+        StringBuffer s11 = new StringBuffer("Vikram");
+        String s12 = "Vikram";
+        String s13 = "Vikram";
 
-            System.out.println(s11.toString().equals(s12.toString()));
+        System.out.println(s11.toString().equals(s12.toString()));
 
         StringBuffer sb1 = new StringBuffer("Hello");
         StringBuffer sb2 = new StringBuffer("Hello");
 
         System.out.println(sb1.equals(sb2));
-        System.out.println(sb1==sb2);
+        System.out.println(sb1 == sb2);
 
-        System.out.println(s12==s13);
+        System.out.println(s12 == s13);
 
-        String str ="Deloitee is a good compnay .Deloitte has a good working culture ";
+        String str = "Deloitee is a good compnay .Deloitte has a good working culture ";
 
         String[] word = str.split(" ");
 
         for (String w : word) {
             System.out.println(w);
         }
-
-
         IntStream.range(0, word.length)
                 .filter(i -> word[i].contains("Deloitte"))
                 .forEach(System.out::println);
 
 
+        Map<Integer, String> mapp = new HashMap<>();
+        mapp.put(1, "Java");
+        mapp.put(2, "Spring");
+        mapp.put(3, "Kafka");
 
 
+        Stream<Integer> mapKeyStream = mapp.keySet().stream();
+        mapKeyStream.forEach(System.out::println);
 
 
+        Stream<Map.Entry<Integer, String>> keyValuePair = mapp.entrySet().stream();
 
+        keyValuePair.forEach(System.out::println);
+
+        mapp.keySet()
+                .stream()
+                .filter(key -> key > 1)
+                .forEach(System.out::println);
+
+
+        mapp.entrySet().stream().
+                filter(e -> e.getValue().startsWith("S"))
+                .map(Map.Entry::getValue)
+                .forEach(System.out::println);
+
+        boolean b = mapp.entrySet().stream().
+                anyMatch(e -> e.getKey().equals(1));
+        System.out.println(b);
+
+
+        List<Integer> arrLit = Arrays.asList(15, 5, 10, 20);
+
+        Integer secondHigh = arrLit.stream().distinct().sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst().orElse(null);
+
+        System.out.println(secondHigh);
+
+
+        String stri = "i love java";
+
+        Map<Character, Long> frequ = stri.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors
+                        .groupingBy(Function.identity(),
+                                Collectors.counting()));
+
+        System.out.println(frequ);
 
 
     }
